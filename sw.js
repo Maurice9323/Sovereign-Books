cd72ea979a9bee623381d4e9a13a4432ffd9ae6d
Maurice9323 <3221sjmo@gmail.com>
2026-08-18 09:35:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-const CACHE = 'sovereign-book-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'sovereign-book-v2';
+const ASSETS = [
+  '/Sovereign-Books/',
+  '/Sovereign-Books/index.html',
+  '/Sovereign-Books/manifest.json',
+  '/Sovereign-Books/icon-192.png',
+  '/Sovereign-Books/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
 
 self.addEventListener('fetch', e => {
